d523e20041f2024104afd57ca381a602e50de7b2
ErnestasPETK <ernestas.petkevicius53@gmail.com>
2022-04-28 21:22:02 +0300

    9 task

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    };
+    wasExpensive() { return this.budget > 100000000 ? true : false }
+
+}
+
+const terminatorMovie = new Movie("Terminator", "me", 100000000000)
+
+console.log(terminatorMovie.wasExpensive())
